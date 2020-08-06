10e37cdf968785f3a9fd5267e4886bb6a5584015
David <davidghazaryan2000@gmail.com>
2020-08-06 04:00:55 +0400

    setup ugly upload endpoint

--- app.js
+++ app.js
@@ -1,9 +1,44 @@
 const express = require('express');
+const upload = require('express-fileupload');
+const fs = require('fs');
 const cors = require('cors');
-
+const path = require('path');
 const config = require('config');
 
 const app = express();
+
+const fileExists = path => {
+    try {
+        fs.statSync(path);
+        return true;
+    }
+    catch (e) {
+        // log(e);
+        return false;
+    }
+}
+
+const createFileIfNotExists = path => {
+    if(!fileExists(fileExists)){
+        fs.closeSync(fs.openSync(path, 'w'));
+    }
+}
+
+const createDirIfNotExists = dir => {
+    if(!fs.existsSync(dir)){
+        fs.mkdirSync(dir);
+    }
+}
+
+const log = data => {
+    const LOG_PATH = path.resolve(__dirname, 'logs/error.log');
+    try{
+        createFileIfNotExists(LOG_PATH);
+        fs.appendFile(LOG_PATH , `Error : ${data}\nDate : ${new Date()}\n`, () => {});
+    }catch (e) { console.log(e) }
+};
+
+app.use(upload());
 app.use(cors());
 
 app.use(express.json({extended : true}));
@@ -12,17 +47,43 @@ app.get('/', (req, res) => {
     res.end(`
         <a href="./about">About</a>
         <br>
-        <a href="./gallery">Gallery</a>
+        <a href="./upload">Upload</a>
         <h1>Home Page</h1>
     `);
 });
 
-app.get('/gallery', (req, res) => {
+app.post('/', (req, res) => {
+    const {file} = req.files;
+    const dir = path.resolve(`uploads/`);
+
+    createDirIfNotExists(dir);
+    
+    file.mv( `${dir}/${file.name}`, err => {
+        if(err){
+            console.log(err);
+            log(err);
+        }
+    })
+
+    res.end(`
+        <h1>Spasi</h1>
+        <a href="../">Home</a>
+    `);
+});
+
+app.get('/upload', (req, res) => {
     res.end(`
         <a href="../">Home</a>
         <br>
         <a href="./about">About</a>
-        <h1>Gallery Page</h1>
+        <h1>Upload Page</h1>
+        <br>
+        <br>
+        <br>
+        <form action="/" method="POST" enctype="multipart/form-data">
+            <input type="file" name="file" />
+            <input type="submit" value="Upload" />
+        </form>
     `);
 });
 
@@ -30,7 +91,7 @@ app.get('/about', (req, res) => {
     res.end(`
         <a href="../">Home</a>
         <br>
-        <a href="./gallery">gallery</a>
+        <a href="./upload">upload</a>
         <h1>About Page</h1>
     `);
 });
