6c246bb8dee8d181ae8ef1e34b4df1f9db2bad3a
David <davidghazaryan2000@gmail.com>
2020-08-06 14:35:20 +0400

    worker test last changes

---
+++ worker/worker.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+
+const fileExists = path => {
+    try {
+        fs.statSync(path);
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+
+const createFileIfNotExists = path => {
+    if (!fileExists(fileExists)) {
+        fs.closeSync(fs.openSync(path, 'w'));
+    }
+}
+
+const createDirIfNotExists = dir => {
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
+}
+const rename = () => {
+    try {
+        const LOG_DIR = path.resolve(`logs/`);
+        createDirIfNotExists(LOG_DIR);
+        const LOGFILE_PATH = `${LOG_DIR}/error.log`;
+        createFileIfNotExists(LOGFILE_PATH);
+        fs.renameSync(LOGFILE_PATH, `${LOG_DIR}/log.log`, err => {
+            if(err) console.log(err)
+        });
+    } catch (e) {
+        console.log(e)
+    }
+};
+rename();
