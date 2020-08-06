53c60263c6b3e8a9f672b8c94d467cc9ab3393ff
David <davidghazaryan2000@gmail.com>
2020-08-06 04:52:33 +0400

    to the same standart with developement config js

--- config/production.js
+++ config/production.js
@@ -1,5 +1,5 @@
 const port = process.env.PORT || 80;
 module.exports = {
   port,
-  "baseUrl" : `http://code-time-automatic.david262929.com/`
+  "baseUrl" : `http://code-time-automatic.david262929.com`
 }
