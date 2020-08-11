48ea692f1a03a3e55651a8af0760e9beda516e7a
David <davidghazaryan2000@gmail.com>
2020-08-11 06:34:45 +0400

    remove old unnecessary files

--- cron.js
+++
@@ -1,9 +0,0 @@
-const cron = require("node-cron")
-const shell = require("shelljs")
-
-cron.schedule("*/5 * * * * *", () => {
-    console.log("Scheduler running...")
-    if (shell.exec("node worker/sayHello.js").code !== 0) {
-        console.log("Something went wrong.");
-    }
-})
