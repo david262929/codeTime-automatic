5ea77fd36984ec23cbb1afcf4ee863c24449398a
David <davidghazaryan2000@gmail.com>
2020-08-11 06:37:36 +0400

    workers && logger tests

--- test.app.js
+++ test.app.js
@@ -1,8 +1,25 @@
+// const log = require('./src/functions/logger');
+//
+// // into ./logs/error_worker_tasks.log >>  "ERROR - blablabla"
+// log( ["ERROR", 'blablabla'].join(' - '), null, 'worker_tasks', 'error' );
+//
+// // into ./logs/error_worker_tasks.log >>  "{_log_log _ 'ERROR - blablabla'}"
+// log( ["ERROR", 'blablabla'].join(' - '), '_log_log', 'worker_tasks', 'error' );
+//
+//
+// // into ./logs/message_worker_tasks.log >>  "MEssage - blablabla"
+// log( ["MEssage", 'blablabla'].join(' - '), null, 'worker_tasks', 'message' );
+//
+// // into ./logs/message_worker_tasks.log >>  "{_log_log _ 'MEssage - blablabla'}"
+// log( ["MEssage", 'blablabla'].join(' - '), '_log_log', 'worker_tasks', 'message' );
 
 
-const log = console.log;
+// const taskQueue = require('./src/functions/queue/redis');
+// const template = { email : 'test'};
+//
+// [1,1,1,1,1].forEach((el, index) => {
+//     template.email += (1 + index)
+//     taskQueue.enqueue(template)
+// })
 
 
-(async () => {
-    console.log( await urlWorking('https://www.google.com' ))
-})()
