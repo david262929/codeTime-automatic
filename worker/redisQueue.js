721e43fedc3e885203cd1e3fd42386d900e6efbd
David <davidghazaryan2000@gmail.com>
2020-08-11 06:03:27 +0400

    removing commented codes

--- worker/redisQueue.js
+++ worker/redisQueue.js
@@ -1,31 +1,3 @@
-// const fs = require('fs');
-// const path = require('path');
-//
-// // const fileExists = path => {
-// //     try {
-// //         fs.statSync(path);
-// //         return true;
-// //     } catch (e) {
-// //         return false;
-// //     }
-// // }
-//
-// const rename = () => {
-//     try {
-//         const LOG_DIR = path.resolve(`logs/`);
-//         createDirIfNotExists(LOG_DIR);
-//         const LOGFILE_PATH = `${LOG_DIR}/error.log`;
-//         createFileIfNotExists(LOGFILE_PATH);
-//         fs.renameSync(LOGFILE_PATH, `${LOG_DIR}/log.log`, err => {
-//             if (err) console.log(err)
-//         });
-//     } catch (e) {
-//         console.log(e)
-//     }
-// };
-// rename();
-//
-
 const {worker: redisQueue} = require("../src/functions/queue/redis");
 const log = require("../src/functions/logger");
 
