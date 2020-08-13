375b182200efd5ee5e39270a6f61751fcaf31817
David <davidghazaryan2000@gmail.com>
2020-08-13 07:09:42 +0400

    tools dir and files schema setup

---
+++ src/functions/tools/taskParser.js
@@ -0,0 +1,16 @@
+
+
+module.exports = {
+    // _makeDir,
+    // fileExists,
+    // dirExists,
+    // createFileIfNotExists,
+    // createDirIfNotExists,
+    // scrapper,
+    // createUploadsTempDir,
+    // haveExtention,
+    // isUrlWorking,
+    // getFilesFromDir,
+    // log,
+    // addTask,
+}
