5f5a80a520be2214952683012f83807696c760cb
David <davidghazaryan2000@gmail.com>
2020-08-15 06:19:48 +0400

    it for alternativly scrapping

--- config/default.js
+++ config/default.js
@@ -4,6 +4,7 @@ const path = require('path');
 module.exports = {
     "port": nodePort,
     "baseUrl": `http://localhost:${nodePort}`,
+    "baseRealUrl": `http://localhost:${nodePort}`,
     "redis" : {
         "host" : `localhost`,
         "port" : redisPort,
